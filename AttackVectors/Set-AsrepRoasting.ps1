364b2fa61f920271549aa29cf84989f9d0466f5f
Bufu <xbufu@pm.me>
2021-09-29 19:47:34 +0200

    added extra parameter and documentation

--- AttackVectors/Set-AsrepRoasting.ps1
+++ AttackVectors/Set-AsrepRoasting.ps1
@@ -1,16 +1,53 @@
 function Set-AsrepRoasting {
 
+    <#
+        .SYNOPSIS
+            Adds ASREP-Roastable users to the domain.
+
+        .DESCRIPTION
+            The function gets a certain amount of random user from the domain and sets the DoesNotRequirePreAuth flag for each. Excludes default accounts like Administrator and krbtgt. Makes 5% of users ASREP-Roastable by default.
+
+        .PARAMETER VulnerableUsersCount
+            The number of ASREP-Roastable users.
+
+        .PARAMETER User
+            The user to make ASREP-Roastable.
+
+        .EXAMPLE
+            PS > Set-AsrepRoasting -Verbose
+
+            Make 5% of users ASREP-Roastable and display verbose output.
+
+        .EXAMPLE
+            PS > Set-AsrepRoasting -VulnerableUsersCount 10
+
+            Make 10 random users in the domain ASREP-Roastable.
+
+        .EXAMPLE
+            PS > Set-AsrepRoasting -User bufu -Verbose
+
+            Make user bufu ASREP-Roastable and display verbose output.
+    #>
+
     param(
         [Parameter(Mandatory=$false, HelpMessage="The number of ASREP-Roastable users.")]
-        [Int]$VulnerableUsersCount = [Int]((Get-ADUser -Filter {(SamAccountName -ne "Administrator") -and (SamAccountName -ne "krbtgt") -and (SamAccountName -ne "guest")} | Measure-Object).Count * (5/100))
+        [Int]$VulnerableUsersCount = [Int]((Get-ADUser -Filter {(SamAccountName -ne "Administrator") -and (SamAccountName -ne "krbtgt") -and (SamAccountName -ne "guest")} | Measure-Object).Count * (5/100)),
+
+        [Parameter(Mandatory=$false, HelpMessage="The user to make ASREP-Roastable.")]
+        [String]$User
     )
 
-    Write-Verbose "Getting ASREP-Roastable users..."
-    
-    $VulnerableUsers = (Get-ADUser -Filter {(SamAccountName -ne "Administrator") -and (SamAccountName -ne "krbtgt") -and (SamAccountName -ne "guest")}).SamAccountName | Sort-Object { Get-Random } | Select -First $VulnerableUsersCount
 
-    foreach($User in $VulnerableUsers) {
-        Write-Verbose "Setting DoesNotRequirePreAuth flag for user: $User"
-        Set-ADAccountControl -Identity $User -DoesNotRequirePreAuth 1    
+    if($User) {
+        $VulnerableUsers = $User
+    } {
+        Write-Verbose "Getting ASREP-Roastable users..."
+
+        $VulnerableUsers = (Get-ADUser -Filter {(SamAccountName -ne "Administrator") -and (SamAccountName -ne "krbtgt") -and (SamAccountName -ne "guest")}).SamAccountName | Sort-Object { Get-Random } | Select -First $VulnerableUsersCount
+    }    
+
+    foreach($Username in $VulnerableUsers) {
+        Write-Verbose "Setting DoesNotRequirePreAuth flag for user: $Username"
+        Set-ADAccountControl -Identity $Username -DoesNotRequirePreAuth 1    
     }
 }
--- AttackVectors/Set-Kerberoasting.ps1
+++ AttackVectors/Set-Kerberoasting.ps1
@@ -1,12 +1,49 @@
 function Set-Kerberoasting {
+    <#
+        .SYNOPSIS
+            Adds Kerberoastable users to the domain.
+
+        .DESCRIPTION
+            The function gets a certain amount of random user from the domain and adds a SPN for each. Excludes default accounts like Administrator and krbtgt. Makes 5% of users kerberoastable by default.
+
+        .PARAMETER VulnerableUsersCount
+            The number of Kerberoastable users.
+
+        .PARAMETER User
+            The user to make kerberoastable.
+
+        .EXAMPLE
+            PS > Set-Kerberoasting -Verbose
+
+            Make 5% of users kerberoastable and display verbose output.
+
+        .EXAMPLE
+            PS > Set-Kerberoasting -VulnerableUsersCount 10
+
+            Make 10 random users in the domain kerberoastable.
+
+        .EXAMPLE
+            PS > Set-Kerberoasting -User bufu -Verbose
+
+            Make user bufu kerberoastable and display verbose output.
+    #>
+
     param(
-        [Parameter(Mandatory=$false, HelpMessage="The number of ASREP-Roastable users.")]
-        [Int]$VulnerableUsersCount = [Int]((Get-ADUser -Filter {(SamAccountName -ne "Administrator") -and (SamAccountName -ne "krbtgt") -and (SamAccountName -ne "guest")} | Measure-Object).Count * (5/100))
+        [Parameter(Mandatory=$false, HelpMessage="The number of Kerberoastable users.")]
+        [Int]$VulnerableUsersCount = [Int]((Get-ADUser -Filter {(SamAccountName -ne "Administrator") -and (SamAccountName -ne "krbtgt") -and (SamAccountName -ne "guest")} | Measure-Object).Count * (5/100)),
+
+        [Parameter(Mandatory=$false, HelpMessage="The user to make kerberoastable.")]
+        [String]$User
     )
 
-    Write-Verbose "Getting Kerberoastable users..."
 
-    $VulnerableUsers = (Get-ADUser -Filter {(SamAccountName -ne "Administrator") -and (SamAccountName -ne "krbtgt") -and (SamAccountName -ne "guest")}).SamAccountName | Sort-Object { Get-Random } | Select -First $VulnerableUsersCount
+    if($User) {
+        $VulnerableUsers = $User
+    } else {
+        Write-Verbose "Getting Kerberoastable users..."
+        
+        $VulnerableUsers = (Get-ADUser -Filter {(SamAccountName -ne "Administrator") -and (SamAccountName -ne "krbtgt") -and (SamAccountName -ne "guest")}).SamAccountName | Sort-Object { Get-Random } | Select -First $VulnerableUsersCount
+    }
 
     foreach($Username in $VulnerableUsers) {
         Write-Verbose "Adding SPN for user: $Username"
